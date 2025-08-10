a3145713d262a04f50919dfd60dfc810902f19a5
Roman <rvpalivoda@gmail.com>
2025-08-10 13:42:44 +0200

    Commit

--- internal/models/balance.go
+++ internal/models/balance.go
@@ -3,21 +3,22 @@ package models
 import (
 	"time"
 
+	"ptop/internal/utils"
+
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
-	"ptop/internal/utils"
 )
 
 type Balance struct {
-	ID           string          `gorm:"primaryKey;size:21"`
-	ClientID     string          `gorm:"size:21;not null"`
+	ID           string          `gorm:"primaryKey;size:21" json:"id"`
+	ClientID     string          `gorm:"size:21;not null" json:"clientID"`
 	Client       Client          `gorm:"foreignKey:ClientID" json:"-"`
-	AssetID      string          `gorm:"size:21;not null"`
+	AssetID      string          `gorm:"size:21;not null" json:"assetID"`
 	Asset        Asset           `gorm:"foreignKey:AssetID" json:"-"`
-	Amount       decimal.Decimal `gorm:"type:decimal(32,8);not null"`
-	AmountEscrow decimal.Decimal `gorm:"type:decimal(32,8);not null"`
-	CreatedAt    time.Time       `gorm:"autoCreateTime"`
-	UpdatedAt    time.Time       `gorm:"autoUpdateTime"`
+	Amount       decimal.Decimal `gorm:"type:decimal(32,8);not null" json:"amount"`
+	AmountEscrow decimal.Decimal `gorm:"type:decimal(32,8);not null" json:"amountEscrow"`
+	CreatedAt    time.Time       `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
 }
 
 func (b *Balance) BeforeCreate(tx *gorm.DB) (err error) {
--- internal/models/wallet.go
+++ internal/models/wallet.go
@@ -9,17 +9,17 @@ import (
 )
 
 type Wallet struct {
-	ID              string    `gorm:"primaryKey;size:21" json:"id"`
-	ClientID        string    `gorm:"size:21;not null;uniqueIndex:idx_wallet_client_asset_active" json:"clientID"`
-	Client          Client    `gorm:"foreignKey:ClientID" json:"-"`
-	AssetID         string    `gorm:"size:21;not null;uniqueIndex:idx_wallet_client_asset_active" json:"assetID"`
-	Asset           Asset     `gorm:"foreignKey:AssetID" json:"-"`
-	Value           string    `gorm:"type:varchar(255);not null"`
-	DerivationIndex uint32    `gorm:"not null" json:"index"`
-	CreatedAt       time.Time `gorm:"autoCreateTime"`
-	IsEnabled       bool      `gorm:"not null;default:true;uniqueIndex:idx_wallet_client_asset_active"`
-	EnabledAt       time.Time `gorm:"autoCreateTime"`
-	DisabledAt      *time.Time
+	ID              string     `gorm:"primaryKey;size:21" json:"id"`
+	ClientID        string     `gorm:"size:21;not null;uniqueIndex:idx_wallet_client_asset_active" json:"clientID"`
+	Client          Client     `gorm:"foreignKey:ClientID" json:"-"`
+	AssetID         string     `gorm:"size:21;not null;uniqueIndex:idx_wallet_client_asset_active" json:"assetID"`
+	Asset           Asset      `gorm:"foreignKey:AssetID" json:"-"`
+	Value           string     `gorm:"type:varchar(255);not null" json:"value"`
+	DerivationIndex uint32     `gorm:"not null" json:"index"`
+	CreatedAt       time.Time  `gorm:"autoCreateTime" json:"createdAt"`
+	IsEnabled       bool       `gorm:"not null;default:true;uniqueIndex:idx_wallet_client_asset_active" json:"-"`
+	EnabledAt       time.Time  `gorm:"autoCreateTime" json:"enabledAt"`
+	DisabledAt      *time.Time `json:"-"`
 }
 
 func (w *Wallet) BeforeCreate(tx *gorm.DB) (err error) {
