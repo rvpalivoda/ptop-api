0bd871560ee2bf9356aa42dd047f9babca4d2cd3
Roman <rvpalivoda@gmail.com>
2025-08-10 18:30:17 +0200

    c

--- internal/models/client.go
+++ internal/models/client.go
@@ -13,11 +13,11 @@ import (
 type Client struct {
 	ID           string          `gorm:"primaryKey;size:21" json:"id"`
 	Username     string          `gorm:"type:varchar(255);not null;unique" json:"username"`
-	PinCode      *string         `gorm:"type:varchar(255)" json:"pinCode"`
+	PinCode      *string         `gorm:"type:varchar(255)" json:"-"`
 	TwoFAEnabled bool            `gorm:"not null;default:false" json:"twoFAEnabled"`
-	TOTPSecret   *string         `gorm:"type:varchar(255)" json:"TOTPSecret"`
-	Bip39        datatypes.JSON  `gorm:"type:json" json:"bip39" swaggertype:"object"`
-	Password     *string         `gorm:"type:varchar(255)"`
+	TOTPSecret   *string         `gorm:"type:varchar(255)" json:"-"`
+	Bip39        datatypes.JSON  `gorm:"type:json" json:"bip39" swaggertype:"object" json:"-"`
+	Password     *string         `gorm:"type:varchar(255)" json:"-"`
 	RegistredAt  time.Time       `gorm:"autoCreateTime"`
 	Rating       decimal.Decimal `gorm:"type:decimal(3,2);not null;default:0" json:"rating"`
 	OrdersCount  int             `gorm:"not null;default:0" json:"ordersCount"`
