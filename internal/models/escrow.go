e50785f088add5855777a327d8fe3fb1a14ad8c8
Roman <rvpalivoda@gmail.com>
2025-08-10 18:25:49 +0200

    c

--- internal/models/escrow.go
+++ internal/models/escrow.go
@@ -3,24 +3,25 @@ package models
 import (
 	"time"
 
+	"ptop/internal/utils"
+
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
-	"ptop/internal/utils"
 )
 
 type Escrow struct {
-	ID        string          `gorm:"primaryKey;size:21"`
-	ClientID  string          `gorm:"size:21;not null"`
+	ID        string          `gorm:"primaryKey;size:21" json:"id"`
+	ClientID  string          `gorm:"size:21;not null" json:"clientID"`
 	Client    Client          `gorm:"foreignKey:ClientID" json:"-"`
-	AssetID   string          `gorm:"size:21;not null"`
+	AssetID   string          `gorm:"size:21;not null" json:"assetID"`
 	Asset     Asset           `gorm:"foreignKey:AssetID" json:"-"`
-	Amount    decimal.Decimal `gorm:"type:decimal(32,8);not null"`
-	OfferID   *string         `gorm:"size:21"`
+	Amount    decimal.Decimal `gorm:"type:decimal(32,8);not null" json:"amount"`
+	OfferID   *string         `gorm:"size:21" json:"offerID"`
 	Offer     Offer           `gorm:"foreignKey:OfferID" json:"-"`
-	OrderID   *string         `gorm:"size:21"`
+	OrderID   *string         `gorm:"size:21" json:"orderID"`
 	Order     Order           `gorm:"foreignKey:OrderID" json:"-"`
-	CreatedAt time.Time       `gorm:"autoCreateTime"`
-	UpdatedAt time.Time       `gorm:"autoUpdateTime"`
+	CreatedAt time.Time       `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
 }
 
 func (e *Escrow) BeforeCreate(tx *gorm.DB) (err error) {
--- internal/models/transaction_in.go
+++ internal/models/transaction_in.go
@@ -3,10 +3,11 @@ package models
 import (
 	"time"
 
+	"ptop/internal/utils"
+
 	"github.com/shopspring/decimal"
 	"gorm.io/datatypes"
 	"gorm.io/gorm"
-	"ptop/internal/utils"
 )
 
 type TransactionInStatus string
@@ -19,19 +20,19 @@ const (
 )
 
 type TransactionIn struct {
-	ID        string              `gorm:"primaryKey;size:21"`
+	ID        string              `gorm:"primaryKey;size:21" json:"id"`
 	ClientID  string              `gorm:"size:21;not null"`
 	Client    Client              `gorm:"foreignKey:ClientID" json:"-"`
 	WalletID  string              `gorm:"size:21;not null"`
 	Wallet    Wallet              `gorm:"foreignKey:WalletID" json:"-"`
-        AssetID   string              `gorm:"size:21;not null"`
-        Asset     Asset               `gorm:"foreignKey:AssetID" json:"-"`
-        AssetName string              `gorm:"->;column:asset_name" json:"assetName"`
-	Amount    decimal.Decimal     `gorm:"type:decimal(32,8);not null"`
-	Status    TransactionInStatus `gorm:"type:varchar(20);not null"`
-        Data      datatypes.JSON      `gorm:"type:json" swaggertype:"object"`
-	CreatedAt time.Time           `gorm:"autoCreateTime"`
-	UpdatedAt time.Time           `gorm:"autoUpdateTime"`
+	AssetID   string              `gorm:"size:21;not null"`
+	Asset     Asset               `gorm:"foreignKey:AssetID" json:"-"`
+	AssetName string              `gorm:"->;column:asset_name" json:"assetName"`
+	Amount    decimal.Decimal     `gorm:"type:decimal(32,8);not null" json:"amount"`
+	Status    TransactionInStatus `gorm:"type:varchar(20);not null" json:"status"`
+	Data      datatypes.JSON      `gorm:"type:json" swaggertype:"object"`
+	CreatedAt time.Time           `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time           `gorm:"autoUpdateTime" json:"updatedAt"`
 }
 
 func (t *TransactionIn) BeforeCreate(tx *gorm.DB) (err error) {
